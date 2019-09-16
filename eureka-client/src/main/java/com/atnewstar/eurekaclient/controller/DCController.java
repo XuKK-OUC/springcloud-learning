189153165c267d1db88ff659a0ca78bf85065e9b
kk <goodluck@qq.com>
2019-09-16 09:56:11 +0800

    eureka-server and enreka-client

---
+++ eureka-client/src/main/java/com/atnewstar/eurekaclient/controller/DCController.java
@@ -0,0 +1,28 @@
+package com.atnewstar.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author Xukaikai
+ * @date 2019/9/16 9:38
+ */
+@RestController
+public class DCController {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/dc")
+    public String dc(){
+        List<String> services = discoveryClient.getServices();
+        StringBuilder stringBuilder = new StringBuilder();
+        for(String s:services){
+            stringBuilder.append(s);
+        }
+        return stringBuilder.toString();
+    }
+}
